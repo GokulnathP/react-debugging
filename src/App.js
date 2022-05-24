677bba651e290646a2b090a96bb60359a1ff86b5
GokulnathP <4gokul2000@gmail.com>
2022-05-24 20:08:10 +0530

    Working counter app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
 import './App.css';
+import {useState} from "react";
 
-function App() {
+const a = () => {
+    console.log("a");
+}
+
+const b = () => {
+    a();
+    console.log("b");
+}
+
+const useCounter = () => {
+    const [count, setCount] = useState(0);
+    const [incrementValue, setIncrementValue] = useState(1);
+
+    const setCounter = (args) => {
+        const {newCount, newIncrementValue} = args;
+        setCount(newCount);
+        setIncrementValue(newIncrementValue);
+    }
+
+    return [{count, incrementValue}, setCounter];
+};
+
+
+const App = () => {
+    const [counter, setCounter] = useCounter();
+
+    b();
+
     return (
-        <div>
-            Welcome to debugging React application
+        <div className="counter">
+            <header>
+                Welcome to debugging React application (Counter)
+            </header>
+            <section>
+                <div className="count">
+                    Count: <b>{counter.count}</b>
+                </div>
+                <div className="actions">
+                    <button
+                        className="reset"
+                        onClick={() => setCounter({newCount: 0, newIncrementValue: 1})}
+                    >
+                        Reset
+                    </button>
+                    <button
+                        className="increment"
+                        onClick={() => setCounter({
+                            newCount: counter.count + counter.incrementValue,
+                            newIncrementValue: counter.incrementValue
+                        })}
+                    >
+                        Increment
+                    </button>
+                </div>
+            </section>
         </div>
     );
 }
